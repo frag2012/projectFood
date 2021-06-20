7eaf489ebe0160b745a068787e0d69b68c940177
AlexFrag <frag2012@ya.ru>
2021-06-20 11:55:47 +0500

    add npm and create db.json

--- js/script.js
+++ js/script.js
@@ -290,6 +290,11 @@ window.addEventListener('DOMContentLoaded', () => {
       }, 4000);
    }
 
+   fetch('http://localhost:3000/menu')
+      .then(data => data.json())
+      .then(res => console.log(res));
+
+
 });
 
 
